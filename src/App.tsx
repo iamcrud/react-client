892b6c414c4b2c6b39c5ac14523b6348eee3ea93
Pau Fracés <pfraces@gmail.com>
2020-09-08 17:25:20 +0200

    renamed file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 
 import { ListModel } from "lists/lists.model";
-import { useLists } from "lists/lists.hooks";
+import { useLists } from "lists/lists.hook";
 
 import { Menu } from "components/menu/menu";
 import { NoListSelected } from "components/no-list-selected/no-list-selected";
