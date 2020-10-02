5b4835fe6a590b4cd028befe5de06b88311dab82
Pau Fracés <pfraces@gmail.com>
2020-10-02 17:56:59 +0200

    added props propagation to header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import styles from "./header.module.scss";
 
-export type HeaderProps = {};
+export type HeaderProps = {
+  className?: string;
+  [key: string]: any;
+};
 
-export const Header = () => (
-  <div className={styles.header}>
+export const Header = ({ className = "", ...props }: HeaderProps) => (
+  <div className={`${className} ${styles.header}`} {...props}>
     <AppBar position="sticky">
       <h1>IamCrud - React Client</h1>
     </AppBar>
